79c9f6fa6eafa7112a8d52c49e4e1e5a3ea7f9c9
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-03-04 22:19:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift("element")
+  return array.unshift(element)
   
   
 }
